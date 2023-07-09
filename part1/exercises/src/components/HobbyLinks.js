359cf9bfe591085d9d914ba4011eda806c710b12
Julia Loftin <julia.loftin7@gmail.com>
2023-07-09 14:17:28 -0500

    completes exercise

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+export default function HobbyLinks(){
+    const hobbyLinks = ['pacmag.com', 'kbdfans.com'];
+
+    return (
+        <div>
+            <a href="{hobbyLinks[0]}">Pc Mag</a>
+            <a href="{hobbyLinks[1]}">Mechanical Keyboards</a>
+        </div>
+    );
+}
